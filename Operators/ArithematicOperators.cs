aef4bb3266229235493f7fb6a16ad71b269f2165
Ankita <ankita.m@softwaysolutions.com>
2019-03-03 18:47:16 +0530

    operators in c#

---
+++ Operators/ArithematicOperators.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class ArithematicOperators
+    {
+        public int  Operators()
+        {
+           Console.WriteLine("enter the first no: ");
+           string a = Console.ReadLine();
+            Console.WriteLine("enter the second no: ");
+            
+            string   b= Console.ReadLine();
+
+            int add = Int32.Parse(a + b);
+            Console.WriteLine("resultant is : " +  add);
+           Console.ReadLine();
+            //int sub = a - b;
+            //int mul = a * b;
+            //int div= a % b;
+            //string res = add + " " + sub + " " + mul + " " + div; 
+            //Console.WriteLine("arithematic operator results are: "+ add + sub + mul + div);
+            //Console.ReadLine();
+            //return (res); 
+            return add;
+            
+        }
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ArithematicOperators modelClass = new ArithematicOperators();
+          modelClass.Operators();
+
+            voteRights();
+        }
+
+        public static int voteRights()
+        {
+            int age = 18;
+            if(age <= 18)
+            {
+                Console.WriteLine("not eligible to vote...");
+            }
+            else 
+            {
+                Console.WriteLine("eligible to vote....");
+            }
+            return age;
+            }
+        }
+    }
+
